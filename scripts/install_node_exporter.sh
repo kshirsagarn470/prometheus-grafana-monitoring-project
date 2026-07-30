416688b049a99121cef38ceee12a35e79364d156
kshirsagarn470 <kshirsagarn470@gmail.com>
2026-07-30 13:04:10 +0530

    Update install_node_exporter.sh

---
+++ scripts/install_node_exporter.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+----> wget https://github.com/prometheus/node_exporter/releases/download/v1.8.1/node_exporter-1.8.1.linux-amd64.tar.gz
+
+----> tar -xvf node_exporter-1.8.1.linux-amd64.tar.gz
+
+----> cd node_exporter-1.8.1.linux-amd64
+
+----> ./node_exporter
+
+----> sudo systemctl daemon-reload
+
+----> sudo systemctl enable node_exporter
+
+----> sudo systemctl start node_exporter
+
+----> sudo systemctl status node_exporter
